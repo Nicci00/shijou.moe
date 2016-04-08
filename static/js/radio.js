e1ba27d5677fda1f6bf6d90783d6d5dc0e7e532a
nicci <armadurapersonal@gmail.com>
2016-04-08 01:48:47 -0300

    missing greek question marks

--- static/js/radio.js
+++ static/js/radio.js
@@ -1,5 +1,6 @@
 var radio = document.getElementById('radio-player');
 var idol_image = document.querySelector("#side-image");
+
 var ws = new WebSocket(socket_url);
 
 radio.volume = 0.7;
@@ -7,66 +8,63 @@ radio.volume = 0.7;
 radio.style.visibility = "hidden";
 
 function timestamp(){
-	return "[" + String(Math.floor(Date.now() / 1000)) + "] ";
+    return "[" + String(Math.floor(Date.now() / 1000)) + "] ";
 }
 
 ws.onopen = function(){
-	console.log(timestamp() + "Connected alright")
-}
+    console.log(timestamp() + "Connected alright");
+};
 
 ws.onmessage = function(event) {
-	console.log(timestamp() + "Got data from server alright");
-	var data = JSON.parse(event.data)
+    console.log(timestamp() + "Got data from server alright");
+    var data = JSON.parse(event.data);
 
-	document.getElementById("art").innerHTML = data.artist;
-	document.getElementById("tit").innerHTML = data.title;
-	document.getElementById("lis").innerHTML = data.listeners;
+    document.getElementById("art").innerHTML = data.artist;
+    document.getElementById("tit").innerHTML = data.title;
+    document.getElementById("lis").innerHTML = data.listeners;
 
-	document.title = String(data.artist + " - " + data.title)
+    document.title = String(data.artist + " - " + data.title);
 };
 
 ws.onclose = function(event){
-	console.log(timestamp() + "y tho")
-}
+    console.log(timestamp() + "y tho");
+};
 
 document.querySelector("#mp3-btn").addEventListener("click", function(){
-	radio.src = "http://shijou.moe:8000/imas-radio-lq.mp3";
-	radio.play();
-	console.log(timestamp() + "Now using mp3 source");
+    radio.src = "http://shijou.moe:8000/imas-radio-lq.mp3";
+    radio.play();
+    console.log(timestamp() + "Now using mp3 source");
 });
 
 document.querySelector("#ogg-btn").addEventListener("click", function(){
-	radio.src = "http://shijou.moe:8000/imas-radio.ogg";
-	radio.play();
-	console.log(timestamp() + "Now using ogg-vorbis source");
+    radio.src = "http://shijou.moe:8000/imas-radio.ogg";
+    radio.play();
+    console.log(timestamp() + "Now using ogg-vorbis source");
 });
-
 
 idol_image.addEventListener("click", function(){
 
-	var spinner = document.querySelector('#spinner');
-	
-	idol_image.style.visibility = 'hidden';
+    var spinner = document.querySelector('#spinner');
+    
+    idol_image.style.visibility = 'hidden';
 
-	spinner.className = 'fa fa-circle-o-notch fa-spin';
-	spinner.style.visibility = '';
+    spinner.className = 'fa fa-circle-o-notch fa-spin';
+    spinner.style.visibility = '';
 
-	var req = new XMLHttpRequest();
-	req.open('GET', '/imas-radio/util/side-image/?base64', true);
+    var req = new XMLHttpRequest();
+    req.open('GET', '/imas-radio/util/side-image/?base64', true);
 
-	req.onload = function() {
+    req.onload = function() {
 
-		if (this.status >= 200 && this.status < 400) {
-			idol_image.src = String(this.response);
-		}
+        if (this.status >= 200 && this.status < 400) {
+            idol_image.src = String(this.response);
+        }
 
-		idol_image.style.visibility = ''; 
+        idol_image.style.visibility = ''; 
 
-		spinner.className = '';
-		spinner.style.visibility = 'hidden';
-	}
+        spinner.className = '';
+        spinner.style.visibility = 'hidden';
+    };
 
-	req.send();
+    req.send();
 });
-
-
